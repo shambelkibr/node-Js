cf9e70153a8d4eab9936007cc8944ffcedc3b7cd
Shambelkibr <shambel5110@gmail.com>
2026-02-26 11:05:58 +0300

    routes add

---
+++ ExpressJS/express-intro/Day_2/router.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { usernameController, searchController } from "./controller.js";
+
+const router = express.Router();
+router.get("/users/:username", usernameController);
+router.get("/search", searchController);
+
+export default router;
--- ExpressJS/express-intro/index.js
+++ ExpressJS/express-intro/index.js
@@ -1,12 +1,35 @@
 import express from "express";
-import { usernameController, searchController } from "./Day_2/controller.js";
+// import { usernameController, searchController } from "./Day_2/controller.js";
 
+import router from "./Day_2/router.js";
 const app = express();
 const PORT = 3000;
 
-app.get("/users/:username", usernameController);
-app.get("/search", searchController);
+// app.get("/users/:username", usernameController);
+// app.get("/search", searchController);
+
+app.use("/", router);
+
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+  res.json({ message: `user ${name} with  email ${email}` });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+  res.json({
+    message: `user ${name} with  email ${email} created sucessfull `,
+  });
+});
+
+app.put("/users/:id", (req, res) => {
+  const userID = req.params.id;
+  const { name, email } = req.body;
+  res.json({
+    message: `user ${userID} updated to name ${name},${email}`,
+  });
+}); 
